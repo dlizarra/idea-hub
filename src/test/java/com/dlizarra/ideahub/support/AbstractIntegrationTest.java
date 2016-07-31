36d731c7d28f1a941ca3c8fbc5dea66f43acc154
David Lizárraga <dlizarra5@gmail.com>
2016-07-31 15:48:57 +0200

    Updated tests configuration with new boot 1.4 test annotations.

--- src/test/java/com/dlizarra/ideahub/support/AbstractIntegrationTest.java
+++ src/test/java/com/dlizarra/ideahub/support/AbstractIntegrationTest.java
@@ -1,19 +1,17 @@
 package com.dlizarra.ideahub.support;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.dlizarra.ideahub.AppConfig;
 import com.dlizarra.ideahub.DatabaseConfig;
-import com.dlizarra.ideahub.SecurityConfig;
 import com.dlizarra.ideahub.IdeaHubProfiles;
+import com.dlizarra.ideahub.SecurityConfig;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { AppConfig.class, DatabaseConfig.class, SecurityConfig.class })
-@IntegrationTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.NONE,
+        classes = { AppConfig.class, DatabaseConfig.class, SecurityConfig.class })
 @ActiveProfiles(IdeaHubProfiles.TEST)
 public abstract class AbstractIntegrationTest {
 
--- src/test/java/com/dlizarra/ideahub/support/AbstractWebIntegrationTest.java
+++ src/test/java/com/dlizarra/ideahub/support/AbstractWebIntegrationTest.java
@@ -1,19 +1,17 @@
 package com.dlizarra.ideahub.support;
 
+import com.dlizarra.ideahub.AppConfig;
+import com.dlizarra.ideahub.DatabaseConfig;
+import com.dlizarra.ideahub.IdeaHubProfiles;
+import com.dlizarra.ideahub.SecurityConfig;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.dlizarra.ideahub.AppConfig;
-import com.dlizarra.ideahub.DatabaseConfig;
-import com.dlizarra.ideahub.SecurityConfig;
-import com.dlizarra.ideahub.IdeaHubProfiles;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { AppConfig.class, DatabaseConfig.class, SecurityConfig.class })
-@WebIntegrationTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = { AppConfig.class, DatabaseConfig.class, SecurityConfig.class })
 @ActiveProfiles(IdeaHubProfiles.TEST)
 public abstract class AbstractWebIntegrationTest {
 
