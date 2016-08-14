4c1664009b87880dce9569880456e9e9abb9c1af
David Lizárraga <dlizarra5@gmail.com>
2016-08-14 19:03:37 +0200

    allow GET png files

--- src/main/java/com/dlizarra/ideahub/SecurityConfig.java
+++ src/main/java/com/dlizarra/ideahub/SecurityConfig.java
@@ -52,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						"/**/*.html",
 						"/**/*.css",
 						"/**/*.js",
+						"/**/*.png",
 						"/api/ideas/**",
 						"/api/users/**"
 					).permitAll()
